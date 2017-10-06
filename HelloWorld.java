8aa552bb00ca1076c84734d088c9fadc57592833
katerenee <32461332+katerenee@users.noreply.github.com>
2017-10-06 12:32:54 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 //there is an error in this file, please fix it.
+package CS3A;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World")
+    System.out.println("Hello World");
   }
 }
